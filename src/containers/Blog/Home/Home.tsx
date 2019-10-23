794550d8d8de15421f1d43ff4f9e9a4749119d90
张鸿宇 <zhanghongyu@zhanghongyus-MacBook-Pro.local>
2019-10-23 16:24:02 +0800

    feat: add blog home page

---
+++ src/containers/Blog/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      123
+    </div>
+  )
+}
+
+export default App
--- src/containers/Router.tsx
+++ src/containers/Router.tsx
@@ -3,5 +3,19 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
 } from 'react-router-dom'
+import BlogHome from './Blog/Home/Home'
+
+const MainRouter: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <BlogHome />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default MainRouter
