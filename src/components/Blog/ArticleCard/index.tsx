f83bafa0d67b1c3c6f9685951465c3e235fc1119
张鸿宇 <zhanghongyu@zhanghongyus-MacBook-Pro.local>
2019-10-24 16:23:48 +0800

    feat: add tag component

--- src/components/Blog/ArticleCard/index.tsx
+++ src/components/Blog/ArticleCard/index.tsx
@@ -11,8 +11,8 @@ const ArticleCard: React.FC = () => {
       <div className="content">这里是内容</div>
       <div className="footer">
         <div className="tags">
-          <Tag>https</Tag>
-          <Tag>CDN</Tag>
+          <Tag color="red">https</Tag>
+          <Tag color="blue">CDN</Tag>
           <Tag>教程</Tag>
         </div>
         <div className="read-article-btn">阅读全文 >></div>
--- src/components/Blog/Tag/index.tsx
+++ src/components/Blog/Tag/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import './style.scss'
 
-const ArticleTag: React.FC = props => {
+interface ITagProps {
+  color?: string;
+}
+interface ITagBackgroundStyle {
+  background?: string;
+}
+
+interface ITagBeforeStyle {
+  borderRightColor?: string;
+}
+
+const ArticleTag: React.FC<ITagProps> = props => {
+  const tagBackgroundStyle: ITagBackgroundStyle = {}
+  const tagBeforeStyle: ITagBeforeStyle = {}
+  if (props.color) {
+    tagBackgroundStyle.background = props.color
+    tagBeforeStyle.borderRightColor = props.color
+  }
   return (
     <div className="article-tag-wrap">
-      <div className="tag">
-        <div className="before" />
-        <div className="after" />
+      <div className="tag" style={tagBackgroundStyle}>
+        <div className="before" style={tagBeforeStyle} />
         {props.children}
       </div>
     </div>
